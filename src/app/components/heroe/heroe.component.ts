487fd1f7d8d0b58bc3f0d9f5d3a6fad0f9327a1c
José Luis Molina Soria <jmolina@dedaloingeniros.com>
2017-11-13 19:45:22 +0100

    Incluido: * Routers. * Heroes. * Heroe * Navbar

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: [],
+  encapsulation: ViewEncapsulation.None
+})
+export class HeroeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
